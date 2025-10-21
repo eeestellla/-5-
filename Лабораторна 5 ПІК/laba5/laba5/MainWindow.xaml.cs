d6bee5f146c6c7756f48e0eab17312e7016af86f
eeestellla <130587110+eeestellla@users.noreply.github.com>
2025-10-21 20:07:12 +0300

    Laba5
    Лабораторна робота 5 з дисципліни Проєктування інтерфейсу користувача

---
+++ Лабораторна 5 ПІК/laba5/laba5/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace laba5
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button btn)
+            {
+                Thickness margin = btn.Margin;
+                txtInfo.Text = $"{btn.Content}: Margin = Left {margin.Left}, Top {margin.Top}, Right {margin.Right}, Bottom {margin.Bottom}";
+            }
+        }
+
+    }
+}
